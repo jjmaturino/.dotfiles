2d5daf3f46b4733b43f627ce7d2161fd6b8aedc0
jjmaturino <47672190+jjmaturino@users.noreply.github.com>
2022-01-25 20:09:51 -0600

    reorganized nvim directory to work for stow sys link command

--- nvim/.config/init.lua
+++
@@ -1,5 +0,0 @@
-require "user.options"  -- nvim editor general configs 
-require "user.keymaps"  -- custom keymaps 
-require "user.plugins"  -- where plugins are installed via Packer
-require "user.colorscheme"  -- colorscheme configs
-require "user.cmp"  -- completion/auto-complete configs
